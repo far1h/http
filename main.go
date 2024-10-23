04f59b4883e9acf89bdeed89d708df22cd4b5640
far1h <farihmuhammad45@gmail.com>
2024-10-23 14:24:01 +0700

    Add a custom writer

--- main.go
+++ main.go
@@ -2,17 +2,26 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
 
+type logWriter struct{}
+
 func main() {
-	resp, err := http.Get("http://google.com")
+	resp, err := http.Get("http://github.com/far1h")
 	if err != nil {
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
-	bs := make([]byte, 99999)
-	resp.Body.Read(bs)
+
+	lw := logWriter{}
+	io.Copy(lw, resp.Body)
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
 	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes:", len(bs))
+	return len(bs), nil
 }
